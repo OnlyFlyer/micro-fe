9b7d041fef0fd216e8ef71d17494a5484df723e1
FlyerWu <scofield90.email@gmail.com>
2024-03-29 22:43:53 +0800

    config vue port

--- sub/vue/vite.config.ts
+++ sub/vue/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  server: {
+    port: 6001,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
