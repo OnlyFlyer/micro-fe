e53d79475bdb93083f1cabb1b25ef1e94c9c16d4
FlyerWu <scofield90.email@gmail.com>
2024-03-29 21:26:48 +0800

    swc config

--- sub/swc/src/main.ts
+++ sub/swc/src/main.ts
@@ -1,8 +1,10 @@
 import './public-path.js';
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import Antd from 'ant-design-vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import { qiankunWindow, renderWithQiankun } from "vite-plugin-qiankun/es/helper";
+
+import App from './App.vue'
 import NotFound from './pages/404/index.vue';
 import DDD from './pages/swc_ddd/index.vue';
 import Home from './pages/swc_home/index.vue';
@@ -29,23 +31,12 @@ let router: any = null;
 function render(props: any) {
   const { container } = props;
   router = createRouter({
-    history: createWebHistory(window.__POWERED_BY_QIANKUN__ ? '/swc' : '/'),
+    history: createWebHistory(qiankunWindow.__POWERED_BY_QIANKUN__ ? '/main/swc' : '/'),
     routes,
   });
-  instance = createApp(App).use(router);
+  instance = createApp(App).use(router).use(Antd);
   instance.mount(container ? container.querySelector('#app') : '#app');
 }
-
-// 独立运行时
-if (!window.__POWERED_BY_QIANKUN__) {
-  render({});
-}
-
-// const app = createApp(App);
-
-// app.use(router);
-
-// app.mount('#app').
 
 export async function bootstrap() {
   console.log('=======swc-vue-app bootstrap=======');
@@ -68,4 +59,28 @@ export async function unmount() {
   instance = null;
   router = null;
   console.log('=======swc-vue-app unmount=======');
+}
+
+/**
+ * 可选生命周期钩子，仅使用 loadMicroApp 方式加载微应用时生效
+ */
+export async function update(props: Record<string, string>) {
+  console.log('=======swc-app update=======');
+  console.log('update props', props);
+  console.log('=======swc-app update=======');
+}
+
+
+// 独立运行时
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render({});
+} else {
+  // 作为子应用运行
+  renderWithQiankun({
+    //生命周期函数
+     mount,
+     bootstrap,
+     update,
+     unmount,
+   });
 }
--- sub/swc/vite.config.ts
+++ sub/swc/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import qiankun from 'vite-plugin-qiankun';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    qiankun('swc', { useDevMode: true }),
+  ],
+  server: {
+    port: 4001,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
 })
